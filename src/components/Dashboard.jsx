331897524283c0f152d04517e8b421d392664b44
disha <dishachhabra173@gmail.com>
2024-01-25 20:51:40 +0530

    graphs added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { LineGraph } from './LineGraph';
+import Piechart from './Piechart';
 
 const Dashboard = () => {
   return (
@@ -132,7 +134,7 @@ const Dashboard = () => {
 
             <div class="row">
                 {/* progess chart */}
-              <div class="col-sm-8 grid-margin">
+              <div class="col-sm-6 grid-margin">
                 <div class="card">
                   <div class="card-body">
                     <h5>Progress bar</h5>
@@ -151,162 +153,26 @@ const Dashboard = () => {
                 </div>
               </div>
                {/* pie chart */}
-              <div class="col-sm-4 grid-margin">
+              <div class="col-sm-6 grid-margin">
                 <div class="card">
                   <div class="card-body">
-                    <h5>pie chart</h5>
-                    <div class="row">
-                      <div class="col-16 col-sm-12 col-xl-16 my-auto">
-                        <div class="d-flex d-sm-block d-md-flex align-items-center">
-                          
-                        </div>
-                        
-                      </div>
-                      <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                        <i class="icon-lg mdi mdi-codepen text-primary ms-auto"></i>
-                      </div>
-                    </div>
+                   <Piechart/>
                   </div>
                 </div>
               </div>
             
              
             </div>
-            <div class="row ">
-              <div class="col-12 grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Order Status</h4>
-                    <div class="table-responsive">
-                      <table class="table">
-                        <thead>
-                          <tr>
-                            <th>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input"/>
-                                </label>
-                              </div>
-                            </th>
-                            <th> Client Name </th>
-                            <th> Order No </th>
-                            <th> Product Cost </th>
-                            <th> Project </th>
-                            <th> Payment Mode </th>
-                            <th> Start Date </th>
-                            <th> Payment Status </th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input"/>
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="assets/images/faces/face1.jpg" alt="image" />
-                              <span class="ps-2">Henry Klein</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> Dashboard </td>
-                            <td> Credit card </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-success">Approved</div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input"/>
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="assets/images/faces/face2.jpg" alt="image" />
-                              <span class="ps-2">Estella Bryan</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> Website </td>
-                            <td> Cash on delivered </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-warning">Pending</div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input"/>
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="assets/images/faces/face5.jpg" alt="image" />
-                              <span class="ps-2">Lucy Abbott</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> App design </td>
-                            <td> Credit card </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-danger">Rejected</div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input"/>
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="assets/images/faces/face3.jpg" alt="image" />
-                              <span class="ps-2">Peter Gill</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> Development </td>
-                            <td> Online Payment </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-success">Approved</div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input"/>
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="assets/images/faces/face4.jpg" alt="image" />
-                              <span class="ps-2">Sallie Reyes</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> Website </td>
-                            <td> Credit card </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-success">Approved</div>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
+            {/* line chart */}
+            <div className="row ">
+              <div className="col-12 grid-margin">
+                <div className="card">
+
+                <div style={{width:"50%",height:"50vh"}}><LineGraph/></div>
+    
+
+                    
+                  
                 </div>
               </div>
             </div>
---
+++ src/components/LineGraph.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler, // 1. Import Filler plugin
+} from "chart.js";
+
+// import faker from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler // 1. Register Filler plugin
+);
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio:false,
+  tension: 0.4,
+  scales: {
+    y: {
+      ticks: {
+        min: 100000,
+        max: 10000,
+        stepSize: 10000,
+      },
+    },
+  },
+};
+
+const labels = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Paid tax",
+      data: [
+        60000, 30000, 50000, 90000, 25000, 30244, 56473, 45733, 90342,
+        21343, 23720, 33323,
+      ],
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgb(255,0,0)",
+      fill: {
+        target: "origin", // Set the fill options
+        above: "rgba(255, 0, 0, 0.3)",
+      },
+    },
+  ],
+};
+
+export function LineGraph() {
+  return <Line options={options} data={data}/>;
+}
