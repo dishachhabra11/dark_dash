2aee6a7e6d02b4ad270a803df74dfd884958d5ec
disha <dishachhabra173@gmail.com>
2024-01-25 15:20:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import SideBar from './components/SideBar';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='container-screen'>
+    <SideBar/>
+    <div className="container-fluid ">
+    <Navbar/>
+    </div>
+    <Dashboard/>
     </div>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+        <nav className="sidebar sidebar-offcanvas" id="sidebar">
+        <div className="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+          {/* <a className="sidebar-brand brand-logo" href="index.html"><img src="assets/images/digitaldoor.png" alt="logo"/></a> */}
+          <h3>DIGITAL DOOR</h3>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default SideBar
